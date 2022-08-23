1a09b725d7bd3d756011b8694bc9ffdf9b1ac0b9
Andre K <akoczka@tugraz.at>
2022-08-23 14:11:29 +0200

    final solution

---
+++ antonPaarApp.Designer.cs
@@ -0,0 +1,187 @@
+﻿namespace AntonPaar
+{
+    partial class antonPaarApp
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(antonPaarApp));
+            this.browse_fille = new System.Windows.Forms.Button();
+            this.pathLabel = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.status_label = new System.Windows.Forms.Label();
+            this.cancelButton = new System.Windows.Forms.Button();
+            this.label5 = new System.Windows.Forms.Label();
+            this.countButton = new System.Windows.Forms.Button();
+            this.progressBar = new System.Windows.Forms.ProgressBar();
+            this.label6 = new System.Windows.Forms.Label();
+            this.resultList = new System.Windows.Forms.ListView();
+            this.label3 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // browse_fille
+            // 
+            this.browse_fille.Location = new System.Drawing.Point(14, 34);
+            this.browse_fille.Name = "browse_fille";
+            this.browse_fille.Size = new System.Drawing.Size(75, 23);
+            this.browse_fille.TabIndex = 0;
+            this.browse_fille.Text = "Browse file";
+            this.browse_fille.UseVisualStyleBackColor = true;
+            this.browse_fille.Click += new System.EventHandler(this.browse_file);
+            // 
+            // pathLabel
+            // 
+            this.pathLabel.AutoSize = true;
+            this.pathLabel.Location = new System.Drawing.Point(11, 91);
+            this.pathLabel.Name = "pathLabel";
+            this.pathLabel.Size = new System.Drawing.Size(0, 13);
+            this.pathLabel.TabIndex = 5;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(11, 72);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(32, 13);
+            this.label2.TabIndex = 6;
+            this.label2.Text = "Path:";
+            // 
+            // status_label
+            // 
+            this.status_label.AutoSize = true;
+            this.status_label.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.status_label.Location = new System.Drawing.Point(62, 199);
+            this.status_label.Name = "status_label";
+            this.status_label.Size = new System.Drawing.Size(0, 13);
+            this.status_label.TabIndex = 7;
+            // 
+            // cancelButton
+            // 
+            this.cancelButton.Location = new System.Drawing.Point(183, 31);
+            this.cancelButton.Name = "cancelButton";
+            this.cancelButton.Size = new System.Drawing.Size(114, 54);
+            this.cancelButton.TabIndex = 10;
+            this.cancelButton.Text = "CANCEL";
+            this.cancelButton.UseVisualStyleBackColor = true;
+            this.cancelButton.Click += new System.EventHandler(this.cancel);
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(12, 9);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(88, 13);
+            this.label5.TabIndex = 11;
+            this.label5.Text = "Step 1: select file";
+            // 
+            // countButton
+            // 
+            this.countButton.Location = new System.Drawing.Point(15, 164);
+            this.countButton.Name = "countButton";
+            this.countButton.Size = new System.Drawing.Size(282, 32);
+            this.countButton.TabIndex = 4;
+            this.countButton.Text = "COUNT";
+            this.countButton.UseVisualStyleBackColor = true;
+            this.countButton.Click += new System.EventHandler(this.count_button);
+            // 
+            // progressBar
+            // 
+            this.progressBar.Location = new System.Drawing.Point(15, 107);
+            this.progressBar.Name = "progressBar";
+            this.progressBar.Size = new System.Drawing.Size(282, 32);
+            this.progressBar.TabIndex = 15;
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(16, 145);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(222, 13);
+            this.label6.TabIndex = 12;
+            this.label6.Text = "Step 2: click COUNT to get word occurances";
+            // 
+            // resultList
+            // 
+            this.resultList.FullRowSelect = true;
+            this.resultList.HideSelection = false;
+            this.resultList.Location = new System.Drawing.Point(16, 224);
+            this.resultList.Name = "resultList";
+            this.resultList.Size = new System.Drawing.Size(281, 480);
+            this.resultList.TabIndex = 16;
+            this.resultList.UseCompatibleStateImageBehavior = false;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.label3.Location = new System.Drawing.Point(16, 199);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(40, 13);
+            this.label3.TabIndex = 17;
+            this.label3.Text = "Status:";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(312, 716);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.resultList);
+            this.Controls.Add(this.progressBar);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.cancelButton);
+            this.Controls.Add(this.status_label);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.pathLabel);
+            this.Controls.Add(this.countButton);
+            this.Controls.Add(this.browse_fille);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximumSize = new System.Drawing.Size(328, 755);
+            this.MinimumSize = new System.Drawing.Size(328, 755);
+            this.Name = "Form1";
+            this.Text = "AntonPaar Word Counter";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button browse_fille;
+        private System.Windows.Forms.Label pathLabel;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label status_label;
+        private System.Windows.Forms.Button cancelButton;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Button countButton;
+        private System.Windows.Forms.ProgressBar progressBar;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.ListView resultList;
+        private System.Windows.Forms.Label label3;
+    }
+}
+
---
+++ antonPaarApp.cs
@@ -0,0 +1,200 @@
+﻿using System;
+using System.ComponentModel;
+using System.IO;
+using System.Linq;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace AntonPaar
+{
+    public partial class antonPaarApp : Form
+    {
+        private String toBeProcessed;
+        private readonly BackgroundWorker processWorker;
+        private Boolean cancelTask = false;
+
+        // Initialize the Form
+        public antonPaarApp()
+        {
+            InitializeComponent();
+
+            resultList.Columns.Add("Word", 140);
+            resultList.Columns.Add("Occurance", 140);
+            resultList.GridLines = true;
+            resultList.View = View.Details;
+            resultList.MultiSelect = true;
+            resultList.FullRowSelect = true;
+
+            processWorker = new BackgroundWorker();
+            processWorker.WorkerSupportsCancellation = true; 
+            processWorker.DoWork +=
+               new DoWorkEventHandler(processFile);
+            processWorker.RunWorkerCompleted += completed;
+            processWorker.WorkerReportsProgress = true;
+
+            countButton.Enabled = false;
+        }
+
+        // This function handles the selection of the file, and is triggerd by the "browse file" button
+        private async void browse_file(object sender, EventArgs e)
+        {
+            OpenFileDialog openFileDialog = new OpenFileDialog();
+            String path = "";
+
+            if (openFileDialog.ShowDialog() == DialogResult.OK)
+            {
+                status_label.Text = "Parsing File, please wait";
+
+                path = openFileDialog.FileName;
+                pathLabel.Text = path;
+            }
+            else return;
+
+            cancelTask = false;
+           
+            Task<string> readText = ReadTextAsync(path);
+            await readText;
+          
+            status_label.Text = "Parsing complete, please click COUNT";
+            toBeProcessed = readText.Result;
+            countButton.Enabled = true;
+
+
+        }
+
+        // This function parses the selected file, and is an async function, so nothing is blocked by this process
+        public async Task<string> ReadTextAsync(string path)
+        {
+            var stream = new FileStream(path, FileMode.Open, FileAccess.Read);
+            var reader = new StreamReader(stream);
+
+            var readTask = reader.ReadToEndAsync();
+
+            MethodInvoker invThread = delegate
+            {
+                progressBar.Maximum = (int)stream.Length;
+            };
+            this.Invoke(invThread);
+
+            // The progress bar is an async task, so that it can progress while the parsing is happening
+            // it uses the position of the stream to show an accurate progress of the parsing
+            var progressTask = Task.Run(async () => 
+            {
+                while (stream.Position < stream.Length)
+                {
+                    if(cancelTask)
+                    {
+                        break;
+                    }
+                    invThread = delegate
+                    {
+                        progressBar.Value = (int)stream.Position;
+                    };
+                    this.Invoke(invThread);
+                }
+            });
+
+            if (cancelTask) return "";
+
+            // wait on async tasks
+            await Task.WhenAll(readTask, progressTask); 
+
+            invThread = delegate
+            {
+                progressBar.Value = (int)stream.Length;
+            };
+            this.Invoke(invThread);
+
+            reader.Dispose();
+            stream.Dispose();
+
+            return readTask.Result;
+        }
+
+        // The count_button function triggers the processing of the parsed file
+        private void count_button(object sender, EventArgs e)
+        {
+            if (!processWorker.IsBusy)
+            {
+                processWorker.RunWorkerAsync();
+            }
+        }
+
+
+        // The processFile function does the main analysis on the parsed text.
+        private void processFile(object sender, DoWorkEventArgs e)
+        {
+            BackgroundWorker worker = sender as BackgroundWorker;
+
+            MethodInvoker invThread = delegate
+            {
+                this.status_label.Text = "Counting words, please wait...";
+            };
+            this.Invoke(invThread);
+
+            String[] words = Regex.Split(toBeProcessed, "[\n\\s]").Where(str => str != String.Empty).ToArray<String>();
+ 
+            var wordsDicFinal = words
+               .GroupBy(p => p)
+               .ToDictionary(p => p.Key, q => q.Count());
+
+            var result = wordsDicFinal.OrderByDescending(p => p.Value);
+
+            invThread = delegate
+            {
+                resultList.Visible = false;
+            };
+            this.Invoke(invThread);
+
+            foreach (var res in result)
+            {
+                if (worker.CancellationPending == true)
+                {
+                    e.Cancel = true;
+                    break;
+                }
+                else
+                {
+                    String[] newItem = new string[]{ res.Key , res.Value.ToString() };
+                    invThread = delegate
+                    {
+                        resultList.Items.Add(new ListViewItem(newItem));
+                    };
+                    this.Invoke(invThread);
+                }
+            }
+            invThread = delegate
+            {
+                resultList.Visible = true;
+                status_label.Text = "Done";
+            };
+            this.Invoke(invThread);
+        }
+
+        // After completion of the processFile function, the "cancel" button is turned into a "reset" button for clarity
+        private void completed(object sender, RunWorkerCompletedEventArgs e)
+        {
+            cancelButton.Text = "RESET";
+        }
+
+        // The "cancel" button cancels any running task, and resets the program to the default values.
+        // The "cancel" button can also function as a "reset" button. 
+        private void cancel(object sender, EventArgs e)
+        {
+            if (processWorker.WorkerSupportsCancellation == true)
+            {
+                processWorker.CancelAsync();
+            }
+            
+            cancelTask = true;
+            status_label.ResetText();
+            pathLabel.ResetText();
+            progressBar.Value=0;
+            status_label.Text = "Canceled";
+            resultList.Items.Clear(); //reset list
+            cancelButton.Text = "CANCEL";
+            countButton.Enabled = false;
+        }
+    }
+}
